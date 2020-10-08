616efb501df5886c28a7ae156278ef7178b94a62
hadyngould <70531210+hadyngould@users.noreply.github.com>
2020-10-08 11:17:42 -0400

    test scan loop

--- student.py
+++ student.py
@@ -1,4 +1,5 @@
 #!/usr/bin python3
+from collections import OrderedDict
 from teacher import PiggyParent
 import sys
 import time
@@ -177,7 +178,9 @@ class Piggy(PiggyParent):
         """Does a 360 scan and returns the number of obstacles it sees"""
         pass
         # do a scan of the area in front of the robot
-        self.scan() 
+        self.scan()
+        #sort the scan data for easier analysis
+        self.scan_data = OrderedDict(sorted(self.scan_data.items()))
         # print the results
         for angle in self.scan_data:
             dist = self.scan_data[angle]
